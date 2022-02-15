1760461be9db1a235dbb1346445ff4b512360cfc
Tom Bartle <tombartle96@gmail.com>
2022-02-15 14:20:32 +1100

    Added colour change on jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,24 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Random = UnityEngine.Random;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : NetworkBehaviour
 {
     private MainControls controls;
     private InputAction move;
     private InputAction jump;
 
     public Rigidbody rb;
-    private Vector3 moveDirection;
-    public float moveSpeed = 10f;
-    public float jumpForce = 100f;
+    private NetworkVariable<Vector3> MoveDirection;
+    public float moveSpeed = 2f;
+    public float jumpForce = 20f;
+
+    private NetworkVariable<Color> MeshColour;
+    public MeshRenderer meshRenderer;
     
     private void Awake()
     {
@@ -24,12 +29,12 @@ public class PlayerMovement : MonoBehaviour
     {
         move = controls.InGame.Move;
         move.Enable();
-        move.performed += Move;
-        move.canceled += Move;
+        move.performed += MoveInput;
+        move.canceled += MoveInput;
 
         jump = controls.InGame.Jump;
         jump.Enable();
-        jump.performed += Jump;
+        jump.started += JumpInput;
     }
 
     private void OnDisable()
@@ -37,19 +42,71 @@ public class PlayerMovement : MonoBehaviour
         move.Disable();
     }
 
-    private void Move(InputAction.CallbackContext obj)
+    private void MoveInput(InputAction.CallbackContext obj)
     {
-        // Converts 2D movement input into 3D vector
-        moveDirection = new Vector3(obj.ReadValue<Vector2>().x, 0, obj.ReadValue<Vector2>().y);
+        if (IsOwner)
+        {
+            // Converts 2D movement input into 3D vector
+            Vector3 newMoveDirection = new Vector3(obj.ReadValue<Vector2>().x, 0, obj.ReadValue<Vector2>().y);
+            
+            if (NetworkManager.Singleton.IsServer)
+            {
+                // Player hosting the server can move as normal
+                MoveDirection.Value = newMoveDirection;
+            }
+            else if (NetworkManager.Singleton.IsClient)
+            {
+                // Players ona client must request to move on the server
+                RequestMoveServerRpc(newMoveDirection);
+            }
+        }
     }
 
-    private void Jump(InputAction.CallbackContext obj)
+    [ServerRpc]
+    private void RequestMoveServerRpc(Vector3 newMoveDirection, ServerRpcParams rpcParams = default)
+    {
+        MoveDirection.Value = newMoveDirection;
+    }
+
+    private void JumpInput(InputAction.CallbackContext obj)
+    {
+        if (IsOwner)
+        {
+            if (NetworkManager.Singleton.IsServer)
+            {
+                Jump();
+                
+                Color randomColour = GetRandomColour();
+                meshRenderer.material.color = randomColour;
+                MeshColour.Value = randomColour;
+            }
+            else if (NetworkManager.Singleton.IsClient)
+            {
+                RequestJumpServerRpc();
+            }
+        }
+    }
+
+    private void Jump()
     {
         rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
     }
 
+    [ServerRpc]
+    private void RequestJumpServerRpc(ServerRpcParams rpcParams = default)
+    {
+        Jump();
+        MeshColour.Value = GetRandomColour();
+    }
+
+    private Color GetRandomColour()
+    {
+        return new Color(Random.value, Random.value, Random.value, 1f);
+    }
+
     private void FixedUpdate()
     {
-        rb.AddForce(moveDirection * moveSpeed, ForceMode.VelocityChange);
+        rb.AddForce(MoveDirection.Value * moveSpeed, ForceMode.VelocityChange);
+        meshRenderer.material.color = MeshColour.Value;
     }
 }
